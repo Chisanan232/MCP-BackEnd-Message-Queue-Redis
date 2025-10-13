f6464b9839c772f66687d154c8cfc3df9ad66520
Chisanan232 <chi10211201@cycu.org.tw>
2025-10-14 07:57:32 +0800

    ➕ Implement the Redis version message queue backend component.

--- slack_mcp_plugin/backends/queue/__init__.py
+++ slack_mcp_plugin/backends/queue/__init__.py
@@ -4,6 +4,6 @@ This package contains message queue backend implementations that integrate
 with the Slack MCP server's plugin system.
 """
 
-from slack_mcp.backends.queue.redis import RedisMessageQueueBackend
+from .redis import RedisMessageQueueBackend
 
 __all__ = ["RedisMessageQueueBackend"]
